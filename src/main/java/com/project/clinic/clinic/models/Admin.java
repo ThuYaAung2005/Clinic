3ffa27477d66513f32400583ad0771e22eb73c45
Thu Ya Aung <thuyaag24@gmail.com>
2023-11-14 21:10:57 +0630

    UI added

--- src/main/java/com/project/clinic/clinic/models/Admin.java
+++ src/main/java/com/project/clinic/clinic/models/Admin.java
@@ -3,4 +3,5 @@ package com.project.clinic.clinic.models;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 
 public class Admin  {
+
 }
--- src/main/java/com/project/clinic/clinic/models/Patient.java
+++ src/main/java/com/project/clinic/clinic/models/Patient.java
@@ -1,4 +1,5 @@
 package com.project.clinic.clinic.models;
 
 public class Patient {
+
 }
