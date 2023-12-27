aa2168d52c91734a5c4fd936b41945132f00cdab
ZoRan <102011370+ThuYaAung2005@users.noreply.github.com>
2023-12-27 17:51:30 +0630

    push error

--- src/main/java/com/project/clinic/clinic/controllers/BookingController.java
+++ src/main/java/com/project/clinic/clinic/controllers/BookingController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.print.Doc;
 import java.awt.print.Book;
